15bddb9ca9dffa50692ab8f32faa6dfa32683989
Joan Gavelán <joangavelan@gmail.com>
2023-02-02 09:26:09 -0500

    modify site consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Joan Gavelán | Front End Developer'
+export const SITE_DESCRIPTION =
+  "Welcome to my personal site! I'm Joan and I'm a frontend developer specialized in building web applications with React. Here you will be able to find basic information about me as well as some of my projects."
