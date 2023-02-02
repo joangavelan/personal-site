800a29df26cfa587043585ab5ce71ae530df6c05
Joan Gavelán <joangavelan@gmail.com>
2023-02-02 09:25:51 -0500

    add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    fontFamily: {},
-    extend: {}
+    extend: {
+      colors: {
+        navyBlue: '#011021',
+        textClear: '#E2E8FF',
+        textDark: '#AEB8DC',
+        blue: '#0080FF'
+      }
+    }
   },
   plugins: []
 }
