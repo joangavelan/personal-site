3c8551528ed8fad34967151b7a61871682bc0ab8
Joan Gavelán <joangavelan@gmail.com>
2023-02-09 12:10:49 -0500

    make email a new constant

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,5 @@
 export const SITE_TITLE = 'Joan Gavelán | Front End Developer'
 export const SITE_DESCRIPTION =
   "Welcome to my personal website! My name is Joan, and I specialize in front-end development with a focus on building web applications using React. On this site, you'll find an overview of my background and a showcase of some of my most recent projects."
+
+export const email = 'joangavelan@gmail.com'
